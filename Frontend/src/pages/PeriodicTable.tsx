eaa4c58dc849f1f1009523c083e20000677755fc
GDavij <gdavijac@gmail.com>
2022-07-21 04:00:56 -0300

    Better React Componentization Folder Structure

---
+++ Frontend/src/pages/PeriodicTable.tsx
@@ -0,0 +1,13 @@
+//## Components
+import Navbar from "../components/Navbar/component";
+import PlutoTable from "../components/PlutoTable/component";
+import ElementBar from "../components/ElementBar/component";
+export default function () {
+  return (
+    <>
+      <Navbar />
+      <PlutoTable />
+      <ElementBar />
+    </>
+  );
+}
