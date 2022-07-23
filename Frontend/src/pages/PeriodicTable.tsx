1a3beb267bbd3866b182d2c3a04e08449062ad43
GDavij <gdavijac@gmail.com>
2022-07-23 14:03:32 -0300

    Add RefBar

--- Frontend/src/pages/PeriodicTable.tsx
+++ Frontend/src/pages/PeriodicTable.tsx
@@ -1,11 +1,13 @@
 //## Components
 import Navbar from "../components/Navbar/component";
+import RefBar from "../components/RefBar/component";
 import PlutoTable from "../components/PlutoTable/component";
 import ElementBar from "../components/ElementBar/component";
 export default function () {
   return (
     <>
       <Navbar />
+      <RefBar />
       <PlutoTable />
       <ElementBar />
     </>
